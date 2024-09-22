a9d356a6554037d16974a45e5a620acdeacac26a
Vika <rudaviktoria0@gmail.com>
2024-09-22 20:41:05 +0300

    make build

---
+++ index.91fbd46a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2,i=e.clientHeight/2,c=t.clientHeight/2,d=t.clientWidth/2;t.style.left=l-d+"px",t.style.top=i-c+"px",e.appendChild(t);
+//# sourceMappingURL=index.91fbd46a.js.map
